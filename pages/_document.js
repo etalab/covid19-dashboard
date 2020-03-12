9130a282813797b0bf414117107989c77943a4dd
tmerlier <theophile.merliere@gmail.com>
2020-03-12 10:39:39 +0100

    Use Source Sans Pro and Evolventa font family

--- pages/_document.js
+++ pages/_document.js
@@ -31,10 +31,41 @@ class MyDocument extends Document {
               position: relative;
             }
 
+            @font-face {
+              font-family: "Source Sans Pro";
+              src: url("/public/fonts/Source Sans Pro/SourceSansPro-Regular.otf");
+              font-weight: 400;
+            }
+
+            @font-face {
+              font-family: "Source Sans Pro";
+              src: url("/public/fonts/Source Sans Pro/SourceSansPro-Bold.otf");
+              font-weight: 800;
+            }
+
+            @font-face {
+              font-family: "Source Sans Pro";
+              src: url("/public/fonts/Source Sans Pro/SourceSansPro-It.otf");
+              font-style: italic;
+            }
+
+            @font-face {
+              font-family: "Evolventa";
+              src: url("/public/fonts/Evolventa/Evolventa-Regular.ttf");
+              font-weight: 400;
+            }
+
+            @font-face {
+              font-family: "Evolventa";
+              src: url("/public/fonts/Evolventa/Evolventa-Bold.ttf");
+              font-weight: 800;
+            }
+
             body {
               padding: 0;
               margin: 0;
               overflow: auto;
+              font-family: "Source Sans Pro", Arial, sans-serif;
             }
           `}</style>
       </html>
