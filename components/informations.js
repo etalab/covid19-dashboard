f70a423ec99e8a1e28bc4b7de93626e438ccd2c7
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-03-24 00:33:55 +0100

    Update content

--- components/informations.js
+++ components/informations.js
@@ -4,18 +4,15 @@ const Informations = React.memo(() => (
   <div className='informations'>
     <h3>Informations</h3>
     <p>
-      L‘information officielle sur la progression de l’épidémie en France est très fragmentée.
+      L‘information officielle sur la progression de l’épidémie en France est consolidée par <a href='https://www.santepubliquefrance.fr'>Santé publique France</a>. L’agence propose un <a href='https://www.santepubliquefrance.fr/maladies-et-traumatismes/maladies-et-infections-respiratoires/infection-a-coronavirus/articles/infection-au-nouveau-coronavirus-sars-cov-2-covid-19-france-et-monde'>point épidémiologique quotidien</a>, qui comprend les chiffres-clés nationaux et le nombre de cas confirmés par région, mais aussi des <a href='https://www.data.gouv.fr/fr/organizations/sante-publique-france/'>données plus précises</a> sur le portail <a href='https://data.gouv.fr'>data.gouv.fr</a>.
     </p>
     <p>
-      <a href='https://www.santepubliquefrance.fr/maladies-et-traumatismes/maladies-et-infections-respiratoires/infection-a-coronavirus/articles/infection-au-nouveau-coronavirus-sars-cov-2-covid-19-france-et-monde'>Santé publique France</a> propose un point épidémiologique quotidien, qui comprend les chiffres-clés nationaux, et le nombre de cas confirmés par région.
+      Les agences régionales de santé et les préfectures publient aussi des bulletins centrés sur leur territoire de compétence.
     </p>
     <p>
-      Les agences régionales de santé et les préfectures publient de leur côté des informations au niveau départemental, ainsi que le nombre de personnes décédées ou en réanimation.
+      Cet outil <a href='https://github.com/opencovid19-fr/dashboard'>libre</a>, développé sous l’impulsion de la <a href='https://www.numerique.gouv.fr/dinum/'>direction interministérielle du numérique</a>, propose une vision consolidée des données officielles disponibles.
     </p>
-    <p>
-      Cet outil contributif tente de proposer une <a href='https://www.data.gouv.fr/fr/datasets/chiffres-cles-concernant-lepidemie-de-covid19-en-france/'>vision consolidée</a> des données officielles disponibles.
-    </p>
-    <p>Pensez aussi à consulter la <a href='https://www.gouvernement.fr/info-coronavirus'>page d’information du Gouvernement</a>.</p>
+    <p>Pensez aussi à consulter la <a href='https://www.gouvernement.fr/info-coronavirus'>page d’information du Gouvernement sur le COVID-19</a>.</p>
 
     <style jsx>{`
       .informations {
