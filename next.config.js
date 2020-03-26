cc52d543e0df50d8916a28b9a12bee3a2760be0e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-03-26 17:13:05 +0100

    Add ability to load local data

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     GOUV: process.env.GOUV,
     DISABLE_FOOTER: process.env.DISABLE_FOOTER,
-    SITE_URL: process.env.SITE_URL
+    SITE_URL: process.env.SITE_URL,
+    LOAD_LOCAL_DATA: process.env.LOAD_LOCAL_DATA
   }
 }
