d889cae6562e33ade84cfcfd13b02658c06a35e5
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-03-27 18:11:48 +0100

    MAJ wording indicateurs

--- components/counters.js
+++ components/counters.js
@@ -8,9 +8,9 @@ const Counters = ({report, previousReport}) => {
   const {casConfirmes, hospitalises, reanimation, deces, gueris} = report || {}
   const details = {
     casConfirmes: 'Nombre cumulé de cas de COVID-19 confirmés par un test positif. <br />Un nouvel indicateur sera bientôt proposé.',
-    gueris: 'Nombre cumulé de personnes ayant été hospitalisées <br />et de retour à domicile en raison de l’amélioration de leur état de santé',
-    deces: 'Nombre cumulé de personnes décédées à l’hôpital',
-    hospitalises: 'Nombre de personnes actuellement hospitalisées',
+    gueris: 'Nombre cumulé de patients ayant été hospitalisées pour COVID-19 <br />et de retour à domicile en raison de l’amélioration de leur état de santé',
+    deces: 'Nombre cumulé de patients atteints de COVID-19 décédées à l’hôpital',
+    hospitalises: 'Nombre de patients actuellement hospitalisées pour COVID-19',
     reanimation: ' Nombre de personnes actuellement en réanimation ou soins intensifs'
   }
 
