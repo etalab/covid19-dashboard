1a69a8ae44e8667a046a59768f5e567a4bf45277
tmerlier <theophile.merliere@gmail.com>
2020-04-07 14:20:15 +0200

    Desktop layout selector

---
+++ components/layout-selector/desktop-layout-selector.js
@@ -0,0 +1,54 @@
+import React, {useContext} from 'react'
+import PropTypes from 'prop-types'
+
+import {ThemeContext} from '../pages'
+
+const DesktopLayoutSelector = ({selected, layouts, selectLayout}) => {
+  const theme = useContext(ThemeContext)
+
+  return (
+    <div className='layout-selector-container'>
+      {layouts.map(layout => (
+        <div
+          key={layout}
+          className={`layout ${selected === layout ? 'selected' : ''}`}
+          onClick={() => selectLayout(layout)}
+        >
+          {layout}
+        </div>
+      ))}
+
+      <style jsx>{`
+        .layout-selector-container {
+          display: flex;
+          justify-content: space-around;
+          padding: 0.5em;
+        }
+
+        .layout {
+          padding: 0.5em;
+          color: white;
+          background-color: ${theme.primary};
+          border-radius: 4px;
+        }
+
+        .layout.selected {
+          background-color: ${theme.secondary};
+        }
+
+        .layout:hover {
+          cursor: pointer;
+          background-color: ${theme.alt};
+        }
+        `}</style>
+    </div>
+  )
+}
+
+DesktopLayoutSelector.propTypes = {
+  selected: PropTypes.string.isRequired,
+  layouts: PropTypes.array.isRequired,
+  selectLayout: PropTypes.func.isRequired
+}
+
+export default DesktopLayoutSelector
--- layouts/desktop.js
+++ layouts/desktop.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useState, useContext} from 'react'
 
 import {AppContext} from '../pages'
 
@@ -10,85 +10,131 @@ import Informations from '../components/informations'
 
 import theme from '../styles/theme'
 import colors from '../styles/colors'
+import LayoutSelector from '../components/layout-selector'
 
 import Drom from '../components/react-map-gl/drom'
 import MapSelector from '../components/map-selector'
 
 const DesktopPage = () => {
   const {selectedLocation, selectedMapIdx, setSelectedMapIdx} = useContext(AppContext)
+  const LAYOUTS = {
+    'Vue d’ensemble': () => (
+      <>
+        <div className='menu'>
+          <Scrollable>
+            <>
+              <Statistics />
+              <Informations />
+            </>
+          </Scrollable>
+        </div>
+
+        <div className='map'>
+          <div className='metropole'>
+            <div className='map-selector'>
+              <MapSelector mapIdx={selectedMapIdx} selectMap={setSelectedMapIdx} />
+            </div>
+            <ReactMapGL code={selectedLocation || 'FR'} />
+          </div>
+          <div className='drom-container'>
+            <Drom />
+          </div>
+        </div>
+
+        <style jsx>{`
+        .menu {
+          z-index: 1;
+          display: flex;
+          flex-direction: column;
+          max-width: ${theme.menuWidth};
+          box-shadow: 0 1px 4px ${colors.lightGrey};
+        }
+
+        .map {
+          display: flex;
+          flex: 1;
+          flex-direction: column;
+          height: 100%;
+        }
+
+        .metropole {
+          flex: 1;
+        }
+
+        .drom-container {
+          display: flex;
+          padding: 0.5em;
+          height: 25%;
+        }
+
+        .map-selector {
+          z-index: 1;
+          position: absolute;
+          background-color: #000000aa;
+          color: #fff;
+          border-radius: 4px;
+          margin: 0.5em;
+        }
+
+        @media (max-width: 1000px) {
+          .drom-container {
+            height: 40%;
+          }
+        }
+
+        @media (max-width: 800px) {
+          .drom-container {
+            height: 50%;
+          }
+        }
+    `}</style>
+      </>
+    ),
+    'Suivi des hospitalisations': () => (
+      <h1>Suivi des hospitalisations</h1>
+    ),
+    'Suivi des tests': () => (
+      <h1>
+        Suivi des tests
+      </h1>
+    )
+  }
+
+  const [selectedLayout, setSelectedLayout] = useState(Object.keys(LAYOUTS)[0])
 
   return (
-    <>
-      <div className='menu'>
+    <div className='desktop-container'>
+      <div className='desktop-header'>
         <DateNav />
-        <Scrollable>
-          <>
-            <Statistics />
-            <Informations />
-          </>
-        </Scrollable>
+        <LayoutSelector
+          selected={selectedLayout}
+          layouts={Object.keys(LAYOUTS)}
+          selectLayout={setSelectedLayout}
+        />
       </div>
 
-      <div className='map'>
-        <div className='metropole'>
-          <div className='map-selector'>
-            <MapSelector mapIdx={selectedMapIdx} selectMap={setSelectedMapIdx} />
-          </div>
-          <ReactMapGL code={selectedLocation || 'FR'} />
-        </div>
-        <div className='drom-container'>
-          <Drom />
-        </div>
+      <div className='desktop-content'>
+        {LAYOUTS[selectedLayout]()}
       </div>
 
       <style jsx>{`
-      .menu {
-        z-index: 1;
-        display: flex;
-        flex-direction: column;
-        max-width: ${theme.menuWidth};
-        box-shadow: 0 1px 4px ${colors.lightGrey};
-      }
-
-      .map {
-        display: flex;
-        flex: 1;
-        flex-direction: column;
-        height: 100%;
-      }
-
-      .metropole {
-        flex: 1;
-      }
-
-      .drom-container {
-        display: flex;
-        padding: 0.5em;
-        height: 25%;
-      }
-
-      .map-selector {
-        z-index: 3;
-        position: absolute;
-        background-color: #000000aa;
-        color: #fff;
-        border-radius: 4px;
-        margin: 0.5em;
-      }
-
-      @media (max-width: 1000px) {
-        .drom-container {
-          height: 40%;
+        .desktop-container {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
         }
-      }
 
-      @media (max-width: 800px) {
-        .drom-container {
-          height: 50%;
+        .desktop-header {
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          box-shadow: 0 1px 4px ${colors.lightGrey};
         }
-      }
+        .desktop-content {
+          display: flex;
+        }
     `}</style>
-    </>
+    </div>
   )
 }
 
