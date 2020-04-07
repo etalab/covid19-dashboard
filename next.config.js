67cf80efe4a6009de52790e25c8918a846a7cacb
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-04-07 10:24:56 +0200

    Remove GOUV & DISABLE_FOOTER in next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 module.exports = {
   env: {
-    GOUV: process.env.GOUV,
-    DISABLE_FOOTER: process.env.DISABLE_FOOTER,
     SITE_URL: process.env.SITE_URL,
     SITE_NAME: process.env.SITE_NAME,
     SITE_DESCRIPTION: process.env.SITE_DESCRIPTION
