d5786a227ea8115473a62cc591990dc15d0ee10e
gllmr <gllmr@tutanota.com>
2020-04-15 18:01:41 +0200

    Add style to popup

--- components/layouts/transfert-popup.js
+++ components/layouts/transfert-popup.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import colors from '../../styles/colors'
+import {ArrowRight} from 'react-feather'
+
 const TransfertPopup = ({data}) => {
   const {patients, start, end, way} = data
   const depart = data.from.name
@@ -10,22 +13,52 @@ const TransfertPopup = ({data}) => {
     return date.toLocaleDateString()
   }
 
+  const startDate = formatDate(start)
+  const endDate = formatDate(end)
+
   return (
     <div className='sumup-container'>
-      <div className='title'>Départ : {depart} ({formatDate(start)})</div>
-      <div className='title'>Arrivée : {arrivee} ({formatDate(end)})</div>
-      <div className='footer'>Moyen de transport : {Array.isArray(way) ? way.map(item => (item + ' ')) : way}</div>
-      <div className='footer'>{patients} patients transférés</div>
+      <div className='title'>{start === end ? 'Le' : 'Du'} {startDate} {start !== end && ' au ' + endDate}</div>
+      <div className='text'>{patients} patients transférés </div>
+      <div><span className='red'>{depart}</span> <ArrowRight style={{verticalAlign: 'sub'}} /> <span className='blue'>{arrivee}</span></div>
+      <div className='footer'>Moyen de transport : <span>{Array.isArray(way) ? way.join(', ') : way}</span></div>
+      <div className='infos'>Cliquez pour plus d’informations</div>
 
       <style jsx>{`
       .title {
         font-weight: bold;
+        text-align: center;
+        padding-bottom: .3em;
+      }
+
+      .text {
+        text-align: center;
+        padding: .5em;
+      }
+
+      .red {
+        color: ${colors.red};
+        font-weight: bold;
+        padding: .5em;
+      }
+
+      .blue {
+        color: ${colors.blue};
+        font-weight: bold;
+        padding: .5em;
+      }
+
+      .infos {
+        font-size: small;
+        font-weight: bold;
+        font-style: italic;
+        padding-top: 1em;
       }
 
       .footer {
         font-size: small;
         font-style: italic;
-        padding: 0.2em 0;
+        padding-top: 1.5em;
       }
     `}</style>
     </div>
