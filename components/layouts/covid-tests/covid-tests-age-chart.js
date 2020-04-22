1d42855aef3e40020ad99d103e23a46c9c6335b4
tmerlier <theophile.merliere@gmail.com>
2020-04-22 15:08:36 +0200

    Add covid tests age chart to statistics

---
+++ components/layouts/covid-tests/covid-tests-age-chart.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {sumBy} from 'lodash'
+import {HorizontalBar} from 'react-chartjs-2'
+
+import colors from '../../../styles/colors'
+
+const classesAge = {
+  E: '75 et plus',
+  D: '65-74 ans',
+  C: '45-64 ans',
+  B: '15-44 ans',
+  A: 'moins de 15 ans'
+}
+
+const options = {
+  scales: {
+    yAxes: [{
+      stacked: true
+    }]
+  }
+}
+
+const getSumOfByAge = (array, index, age) => {
+  return sumBy(array.map(item => item[index]), r => r ? r.find(entry => entry.age === age).value : 0)
+}
+
+const formatData = data => {
+  const datasets = [
+    {
+      label: 'testés positifs',
+      data: Object.keys(classesAge).map(classe => getSumOfByAge(data, 'testsPositifsDetails', classe)),
+      backgroundColor: colors.red
+    },
+    {
+      label: 'testés',
+      data: Object.keys(classesAge).map(classe => getSumOfByAge(data, 'testsRealisesDetails', classe)),
+      backgroundColor: colors.lightGrey
+    }
+  ]
+
+  return {
+    labels: Object.keys(classesAge).map(classe => classesAge[classe]),
+    datasets
+  }
+}
+
+const CovidTestsAgeChart = ({reports}) => {
+  return (
+    <HorizontalBar data={formatData(reports)} opitions={options} />
+  )
+}
+
+CovidTestsAgeChart.propTypes = {
+  reports: PropTypes.array.isRequired
+}
+
+export default CovidTestsAgeChart
--- components/layouts/covid-tests/covid-tests-statistics.js
+++ components/layouts/covid-tests/covid-tests-statistics.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import CovidTestsAgeChart from './covid-tests-age-chart'
+
+import {getReport} from '../../../lib/data'
+
+import {AppContext} from '../../../pages'
 
 const CovidTestsStatistics = () => {
+  const {date, forcedDate, selectedLocation} = useContext(AppContext)
+
+  const selectedDate = date || forcedDate
+  const report = getReport(selectedDate, selectedLocation || 'FRA')
+
   return (
-    <h1>Statistics</h1>
+    <div>
+      <CovidTestsAgeChart reports={report.history.filter(r => selectedDate >= r.date)} />
+    </div>
   )
 }
 
