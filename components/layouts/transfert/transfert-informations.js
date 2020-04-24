23a8eb51c8e7e9a50ad756b0ea5c49edb74d85bd
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-04-24 17:31:26 +0200

    Update wording for transferts

--- components/layouts/transfert/transfert-informations.js
+++ components/layouts/transfert/transfert-informations.js
@@ -6,7 +6,10 @@ const TransfertInformations = React.memo(() => (
   <div className='informations'>
     <h3>Informations</h3>
     <p>
-      Données concernant les patients transférés d’une région française à une autre autre, ou vers un autre pays européen.
+      Pour répondre à la mise en tension du système de santé français, le gouvernement opère depuis le 18 mars 2020 des transferts de patients entre établissements hospitaliers. Ce dispositif a pour objectif de désengorger les établissements les plus touchés par l’épidémie de COVID-19.
+    </p>
+    <p>
+      Cet outil propose une vision consolidée des transferts réalisés. Les <a href='https://www.data.gouv.fr/fr/datasets/donnees-relatives-aux-transferts-de-patients-atteints-de-covid-19/' target='_top'>données de transferts de patients</a> sont disponibles sur la plateforme www.data.gouv.fr.
     </p>
 
     <style jsx>{`
