359ef7a0feb07a758f09cb4458c924a47bae726c
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-04-29 21:51:53 +0200

    Fix typo

--- components/counters.js
+++ components/counters.js
@@ -13,13 +13,13 @@ const Counters = props => {
   const previousTotalDeces = (previousReport.deces || 0) + (previousReport.decesEhpad || 0)
 
   const details = {
-    casConfirmes: 'Nombre cumulé de cas de COVID-19 confirmés par un test positif. <br />⚠️ Données sont en cours de consolidation.',
+    casConfirmes: 'Nombre cumulé de cas de COVID-19 confirmés par un test positif. <br />⚠️ Données en cours de consolidation.',
     gueris: 'Nombre cumulé de patients ayant été hospitalisés pour COVID-19 <br />et de retour à domicile en raison de l’amélioration de leur état de santé',
     deces: 'Nombre cumulé de décès de patients hospitalisés pour COVID-19 depuis le 1er mars 2020',
     hospitalises: 'Nombre de patients actuellement hospitalisés pour COVID-19',
     reanimation: ' Nombre de patients actuellement en réanimation ou soins intensifs',
     decesEhpad: 'Nombre cumulé de décès en EHPAD et EMS (établissements médico-sociaux)',
-    casConfirmesEhpad: 'Nombre de cas confirmés par test PCR en EHPAD et EMS.<br />Ce chiffre est inclus dans le nombre total de cas cumulés.',
+    casConfirmesEhpad: 'Nombre de cas confirmés par test PCR en EHPAD et EMS.<br />Ce chiffre est inclus dans le nombre total de cas confirmés.',
     casEhpad: 'Nombre total de cas en EHPAD et EMS'
   }
 
