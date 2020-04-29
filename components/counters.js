330b59c17f4b0b5b77580de5f4710425bbe9d43d
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-04-29 21:48:23 +0200

    Changements en accord avec la DGS

--- components/counters.js
+++ components/counters.js
@@ -6,21 +6,21 @@ import colors from '../styles/colors'
 import Counter from './counter'
 
 const Counters = props => {
-  const {casConfirmes, hospitalises, reanimation, deces, decesEhpad, gueris, casPossiblesEhpad, casConfirmesEhpad} = props.report || {}
+  const {casConfirmes, hospitalises, reanimation, deces, decesEhpad, gueris, casEhpad, casConfirmesEhpad} = props.report || {}
 
   const totalDeces = (deces || 0) + (decesEhpad || 0)
   const previousReport = props.previousReport || {}
   const previousTotalDeces = (previousReport.deces || 0) + (previousReport.decesEhpad || 0)
 
   const details = {
-    casConfirmes: 'Nombre cumulé de cas de COVID-19 confirmés par un test positif. <br />Un nouvel indicateur sera bientôt proposé.',
+    casConfirmes: 'Nombre cumulé de cas de COVID-19 confirmés par un test positif. <br />⚠️ Données sont en cours de consolidation.',
     gueris: 'Nombre cumulé de patients ayant été hospitalisés pour COVID-19 <br />et de retour à domicile en raison de l’amélioration de leur état de santé',
     deces: 'Nombre cumulé de décès de patients hospitalisés pour COVID-19 depuis le 1er mars 2020',
     hospitalises: 'Nombre de patients actuellement hospitalisés pour COVID-19',
     reanimation: ' Nombre de patients actuellement en réanimation ou soins intensifs',
     decesEhpad: 'Nombre cumulé de décès en EHPAD et EMS (établissements médico-sociaux)',
     casConfirmesEhpad: 'Nombre de cas confirmés par test PCR en EHPAD et EMS.<br />Ce chiffre est inclus dans le nombre total de cas cumulés.',
-    casPossiblesEhpad: 'Nombre de cas possibles en EHPAD et EMS, présentant des symptômes compatibles avec un diagnostic COVID-19'
+    casEhpad: 'Nombre total de cas en EHPAD et EMS'
   }
 
   return (
@@ -38,8 +38,8 @@ const Counters = props => {
       </div>
       <div className='title'>Données EHPAD et EMS</div>
       {decesEhpad && <div className='counters'>
+        <Counter value={casEhpad} previousValue={previousReport.casEhpad} label='cas total en EHPAD et EMS' details={details.casEhpad} color='orange' />
         <Counter value={casConfirmesEhpad} previousValue={previousReport.casConfirmesEhpad} label='cas confirmés en EHPAD et EMS' details={details.casConfirmesEhpad} color='darkOrange' />
-        <Counter value={casPossiblesEhpad} previousValue={previousReport.casPossiblesEhpad} label='cas probables en EHPAD et EMS' details={details.casPossiblesEhpad} color='orange' />
         <Counter value={decesEhpad} previousValue={previousReport.decesEhpad} label='décès en EHPAD et EMS' details={details.decesEhpad} color='darkRed' />
       </div>}
 
