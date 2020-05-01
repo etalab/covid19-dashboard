78430cf40a06c96172ad420edf964e6080b73dbd
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-05-01 14:02:31 +0200

    Move tests layers

--- components/layouts/covid-tests/covid-tests-maps.js
+++ components/layouts/covid-tests/covid-tests-maps.js
@@ -1,9 +1,66 @@
-import {
-  testsRealisesLayer,
-  testsRealisesCountLayer,
-  testsPositifsLayer,
-  testsPositifsCountLayer
-} from '../../react-map-gl/layers'
+import colors from '../../styles/colors'
+
+const testsRealisesLayer = {
+  id: 'testsRealises',
+  type: 'circle',
+  source: 'testsRealises',
+  filter: ['>', 'testsRealises', 0],
+  paint: {
+    'circle-opacity': 0.6,
+    'circle-color': colors.darkGrey,
+    'circle-radius': [
+      'interpolate',
+      ['linear'],
+      ['sqrt', ['number', ['get', 'testsRealises']]],
+      0,
+      10,
+      100,
+      70
+    ]
+  }
+}
+
+const testsRealisesCountLayer = {
+  id: 'testsRealises-count',
+  type: 'symbol',
+  source: 'testsRealises',
+  filter: ['>', 'testsRealises', 0],
+  layout: {
+    'text-field': '{testsRealises}',
+    'text-size': 16
+  }
+}
+
+const testsPositifsLayer = {
+  id: 'testsPositifs',
+  type: 'circle',
+  source: 'testsPositifs',
+  filter: ['>', 'testsPositifs', 0],
+  paint: {
+    'circle-opacity': 0.6,
+    'circle-color': colors.red,
+    'circle-radius': [
+      'interpolate',
+      ['linear'],
+      ['sqrt', ['number', ['get', 'testsPositifs']]],
+      0,
+      10,
+      100,
+      70
+    ]
+  }
+}
+
+export const testsPositifsCountLayer = {
+  id: 'testsPositifs-count',
+  type: 'symbol',
+  source: 'testsPositifs',
+  filter: ['>', 'testsPositifs', 0],
+  layout: {
+    'text-field': '{testsPositifs}',
+    'text-size': 16
+  }
+}
 
 const maps = [
   {
