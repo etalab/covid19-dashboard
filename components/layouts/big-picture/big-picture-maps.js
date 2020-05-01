d5cf9f39a5839a6f4766642c6bc2bee617075a7e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-05-01 14:51:15 +0200

    Fix path error

--- components/layouts/big-picture/big-picture-maps.js
+++ components/layouts/big-picture/big-picture-maps.js
@@ -1,4 +1,4 @@
-import colors from '../../styles/colors'
+import colors from '../../../styles/colors'
 
 const decesLayer = {
   id: 'deces',
--- components/layouts/covid-tests/covid-tests-maps.js
+++ components/layouts/covid-tests/covid-tests-maps.js
@@ -1,4 +1,4 @@
-import colors from '../../styles/colors'
+import colors from '../../../styles/colors'
 
 const testsRealisesLayer = {
   id: 'testsRealises',
