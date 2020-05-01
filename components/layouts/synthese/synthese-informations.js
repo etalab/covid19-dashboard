38063d9c6023dcadaf59996a4c99bf1a42e95a62
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-05-01 21:07:09 +0200

    Update wording

--- components/layouts/synthese/synthese-informations.js
+++ components/layouts/synthese/synthese-informations.js
@@ -7,11 +7,10 @@ const SyntheseInformations = React.memo(() => (
     <h3>Informations</h3>
     <p>Cette carte synthétise les 2 indicateurs suivants :</p>
     <ul>
-      <li><b>la circulation du virus</b>, basée sur la proportion de passages aux urgences pour une suspicion de COVID-19</li>
-      <li><b>la tension hospitalière sur les capacités en réanimation</b>, basée sur les taux d’occupation des lits de réanimation par des patients atteints de COVID-19, par rapport à la capacité initiale avant l’épidémie</li>
+      <li><b>la circulation active du virus</b>, basée sur la proportion de passages aux urgences pour une suspicion de COVID-19 ;</li>
+      <li><b>la tension hospitalière sur les capacités en réanimation</b>, basée sur les taux d’occupation des lits de réanimation par des patients atteints de COVID-19, par rapport à la capacité initiale avant l’épidémie.</li>
     </ul>
-    <p>Elle sera actualisée tous les soirs, à l’issue du point presse du Pr Jérôme Salomon.</p>
-    <p>Le statut de chaque département (vert/orange/rouge) est disponible sur <a href='https://www.data.gouv.fr/fr/datasets/indicateurs-dactivite-epidemique-covid-19-par-departement/' target='_top'>www.data.gouv.fr</a>.</p>
+    <p>Le statut de chaque département (vert/orange/rouge) est publié en open data sur <a href='https://www.data.gouv.fr/fr/datasets/indicateurs-dactivite-epidemique-covid-19-par-departement/' target='_top'>www.data.gouv.fr</a>.</p>
 
     <style jsx>{`
       .informations {
