c65a5e631b627f3f6c91c37f77f43d1c7814d8f3
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-05-01 20:30:56 +0200

    Update wording

--- components/layouts/synthese/synthese-informations.js
+++ components/layouts/synthese/synthese-informations.js
@@ -5,12 +5,12 @@ import colors from '../../../styles/colors'
 const SyntheseInformations = React.memo(() => (
   <div className='informations'>
     <h3>Informations</h3>
-    <p>Le suivi de l’activité épidémique repose sur les 2 indicateurs suivants :</p>
+    <p>Cette carte synthétise les 2 indicateurs suivants :</p>
     <ul>
       <li><b>la circulation du virus</b>, basée sur la proportion de passages aux urgences pour une suspicion de COVID-19</li>
-      <li><b>la tension sur les capacités en réanimation</b>, basée sur les taux d’occupation des lits de réanimation par des patients atteints de COVID-19, par rapport à la capacité initiale avant l’épidémie</li>
+      <li><b>la tension hospitalière sur les capacités en réanimation</b>, basée sur les taux d’occupation des lits de réanimation par des patients atteints de COVID-19, par rapport à la capacité initiale avant l’épidémie</li>
     </ul>
-    <p>Cette carte synthétise ces 2 indicateurs et sera actualisée tous les soirs, après le point presse du Pr Jérôme Salomon.</p>
+    <p>Elle sera actualisée tous les soirs, à l’issue du point presse du Pr Jérôme Salomon.</p>
     <p>Le statut de chaque département (vert/orange/rouge) est disponible sur <a href='https://www.data.gouv.fr/fr/datasets/indicateurs-dactivite-epidemique-covid-19-par-departement/' target='_top'>www.data.gouv.fr</a>.</p>
 
     <style jsx>{`
