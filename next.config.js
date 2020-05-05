afafbf9e102381d6590ee0962508defaac4a9b76
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-05-05 19:09:06 +0200

    Replace regions & departements Routes by LAYOUTS name

--- next.config.js
+++ next.config.js
@@ -1,28 +1,5 @@
 require('dotenv').config()
 
-const regions = require('@etalab/decoupage-administratif/data/regions.json')
-const departements = require('@etalab/decoupage-administratif/data/departements.json')
-
-const regionsRoutes = regions.reduce((routes, region) => {
-  routes[`/regions/${region.code}`] = {
-    page: '/',
-    query: {
-      location: `REG-${region.code}`
-    }
-  }
-  return routes
-}, {})
-
-const departementsRoutes = departements.reduce((routes, departement) => {
-  routes[`/departements/${departement.code}`] = {
-    page: '/',
-    query: {
-      location: `DEP-${departement.code}`
-    }
-  }
-  return routes
-}, {})
-
 module.exports = {
   env: {
     SITE_URL: process.env.SITE_URL,
@@ -33,9 +10,10 @@ module.exports = {
   exportPathMap() {
     return {
       '/': {page: '/'},
-      '/activite-epidemique': {page: '/', query: {layout: 'synthese'}},
-      ...departementsRoutes,
-      ...regionsRoutes
+      '/vue-d-ensemble': {page: '/', query: {layout: 'big-picture'}},
+      '/suivi-des-tests': {page: '/', query: {layout: 'tests'}},
+      '/transferts-de-patients': {page: '/', query: {layout: 'transferts'}},
+      '/activite-epidemique': {page: '/', query: {layout: 'synthese'}}
     }
   }
 }
