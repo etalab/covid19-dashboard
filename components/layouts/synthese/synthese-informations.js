75820f5c67e7be3056a9c52af4d050873c020d3b
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-05-07 19:24:59 +0200

    Update wording

--- components/layouts/synthese/synthese-informations.js
+++ components/layouts/synthese/synthese-informations.js
@@ -8,7 +8,8 @@ const SyntheseInformations = React.memo(() => (
     <p>Cette carte synthétise, depuis le 30 avril 2020, les 2 indicateurs suivants :</p>
     <ul>
       <li><b>la circulation active du virus</b>, basée sur la proportion de passages aux urgences pour une suspicion de COVID-19 ;</li>
-      <li><b>la tension hospitalière sur les capacités en réanimation</b>, basée sur les taux d’occupation des lits de réanimation par des patients atteints de COVID-19, par rapport à la capacité initiale avant l’épidémie.</li>
+      <li><b>la tension hospitalière sur les capacités en réanimation</b>, basée sur les taux d’occupation des lits de réanimation par des patients atteints de COVID-19, par rapport à la capacité initiale avant l’épidémie ;</li>
+      <li><b>le taux de couverture des besoins en tests</b> estimés au 11 mai.</li>
     </ul>
     <p>Le statut de chaque département (vert/orange/rouge) est publié en open data sur <a href='https://www.data.gouv.fr/fr/datasets/indicateurs-dactivite-epidemique-covid-19-par-departement/' target='_top'>www.data.gouv.fr</a>.</p>
 
