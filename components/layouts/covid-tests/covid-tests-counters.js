5842d9c1e98d719eeab47bb6ec3340bc587a03d2
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-05-19 16:15:16 +0200

    Make CovidTestsCounters clickable

--- components/layouts/covid-tests/covid-tests-counters.js
+++ components/layouts/covid-tests/covid-tests-counters.js
@@ -1,17 +1,55 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PropTypes from 'prop-types'
+import {keyBy} from 'lodash'
 
 import Counter from '../../counter'
 
 import colors from '../../../styles/colors'
 
+import map from './covid-tests-maps'
+
+import {CovidTestsChartContext} from './covid-tests-statistics'
+import {CovidTestsContext} from '.'
+
 const CovidTestsCounters = ({testsPositifs, testsRealises}) => {
+  const {selectedChart, setSelectedChart} = useContext(CovidTestsChartContext)
+  const {setSelectedMapId} = useContext(CovidTestsContext)
+
+  const mapProperties = keyBy(map, 'property')
+
+  const handleClick = chartName => {
+    if (chartName === selectedChart) {
+      setSelectedChart('mixed')
+      setSelectedMapId(mapProperties.testsPositifs.name)
+    } else {
+      setSelectedChart(chartName)
+
+      if (mapProperties[chartName]) {
+        setSelectedMapId(mapProperties[chartName].name)
+      }
+    }
+  }
+
   return (
     <div className='stats'>
       <div className='title'>Tests PCR en laboratoires de ville</div>
       <div className='counters'>
-        <Counter value={testsRealises} label='Tests réalisés' details='Nombre total de tests pour recherche de SARS-COV-2 ' color='darkGrey' />
-        <Counter value={testsPositifs} label='Tests positifs' details='Nombre total de tests positifs pour recherche de SARS-COV-2 à ce jour' color='red' />
+        <Counter
+          isSelected={selectedChart === 'testsRealises'}
+          onClick={() => handleClick('testsRealises')}
+          value={testsRealises}
+          label='Tests réalisés'
+          details='Nombre total de tests pour recherche de SARS-COV-2'
+          color='darkGrey'
+        />
+        <Counter
+          isSelected={selectedChart === 'testsPositifs'}
+          onClick={() => handleClick('testsPositifs')}
+          value={testsPositifs}
+          label='Tests positifs'
+          details='Nombre total de tests positifs pour recherche de SARS-COV-2 à ce jour'
+          color='red'
+        />
       </div>
 
       <style jsx>{`
