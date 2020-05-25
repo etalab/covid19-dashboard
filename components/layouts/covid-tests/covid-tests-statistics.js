8b1020c61cfe5fc829c9828de2aab456272e617f
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-05-25 14:45:40 +0200

    Add button to show mixed charts

--- components/layouts/covid-tests/covid-tests-statistics.js
+++ components/layouts/covid-tests/covid-tests-statistics.js
@@ -132,6 +132,10 @@ const CovidTestsStatistics = () => {
           <div className='chart-container'>
             <Chart reports={report.history.filter(r => selectedDate >= r.date)} {...chartOptions(selectedStat)} />
           </div>
+          {selectedStat !== 'mixed' &&
+            <div className='mixed-chart-container'>
+              <div className='mixed-chart-button' onClick={() => setSelectedStat('mixed')}>Afficher le cumul</div>
+            </div>}
         </>
       )}
       {report && <CovidTestsAgeChart reports={report.history.filter(r => selectedDate >= r.date)} />}
@@ -177,6 +181,35 @@ const CovidTestsStatistics = () => {
 
         .chart-container {
           margin: ${isMobileDevice ? '0 0.2em' : '0 1em'};
+        }
+
+        .mixed-chart-container {
+          border-bottom: 1px solid ${colors.darkBlue};
+          border-right: 1px solid ${colors.darkBlue};
+          margin: ${isMobileDevice ? '0.5em 0.2em' : '1em'};
+        }
+
+        .mixed-chart-button {
+          display: block;
+          font-weight: bold;
+          height: 100%;
+          text-align: center;
+          background-color: ${colors.white};
+          color: ${colors.darkBlue};
+          padding: 0.4em;
+          font-size: .7em;
+          letter-spacing: .1em;
+          border: 1px solid ${colors.darkBlue};
+          text-transform: uppercase;
+          transform: translate(-.1em, -.1em);
+          transition: transform .1s ease-out;
+        }
+
+        .mixed-chart-button:hover {
+          cursor: pointer;
+          color: ${colors.white};
+          background-color: ${colors.darkBlue};
+          transform: translate(0px, 0px);
         }
         `}</style>
     </>
