db600aca6579f65fb7c2eb71f1b8217761c122d7
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-05-25 17:15:26 +0200

    Add Button component to BigPicture & CovidTests

--- components/layouts/big-picture/big-picture-statistics.js
+++ components/layouts/big-picture/big-picture-statistics.js
@@ -12,6 +12,7 @@ import Counters from './big-picture-counters'
 import MixedChart from '../../charts/mixed-chart'
 import IndicateurCumulChart from '../../charts/indicateur-cumul'
 import IndicateurVariationChart from '../../charts/indicateur-variation'
+import Button from '../../button'
 
 import {BigPictureContext} from '.'
 
@@ -175,9 +176,7 @@ const BigPictureStatistics = () => {
             <Chart reports={report.history.filter(r => date >= r.date)} {...selectedChartOptions} />
           </div>
           {selectedStat !== 'mixed' &&
-            <div className='mixed-chart-container'>
-              <div className='mixed-chart-button' onClick={() => setSelectedStat('mixed')}>Afficher le cumul</div>
-            </div>}
+            <Button title='Afficher le cumul' onClick={() => setSelectedStat('mixed')} isMobileDevice={isMobileDevice} />}
         </>
       )}
 
@@ -224,35 +223,6 @@ const BigPictureStatistics = () => {
           text-align: right;
           font-size: 0.8em;
           cursor: pointer;
-        }
-
-        .mixed-chart-container {
-          border-bottom: 1px solid ${colors.darkBlue};
-          border-right: 1px solid ${colors.darkBlue};
-          margin: ${isMobileDevice ? '0.5em 0.2em' : '0.5em 1em'};
-        }
-
-        .mixed-chart-button {
-          display: block;
-          font-weight: bold;
-          height: 100%;
-          text-align: center;
-          background-color: ${colors.white};
-          color: ${colors.darkBlue};
-          padding: 0.4em;
-          font-size: .7em;
-          letter-spacing: .1em;
-          border: 1px solid ${colors.darkBlue};
-          text-transform: uppercase;
-          transform: translate(-.1em, -.1em);
-          transition: transform .1s ease-out;
-        }
-
-        .mixed-chart-button:hover {
-          cursor: pointer;
-          color: ${colors.white};
-          background-color: ${colors.darkBlue};
-          transform: translate(0px, 0px);
         }
         `}</style>
     </>
--- components/layouts/covid-tests/covid-tests-statistics.js
+++ components/layouts/covid-tests/covid-tests-statistics.js
@@ -17,6 +17,7 @@ import IndicateurVariationChart from '../../charts/indicateur-variation'
 import CovidTestsMixedChart from '../../charts/covid-tests-mixed-chart'
 
 import {CovidTestsContext} from '.'
+import Button from '../../button'
 
 const charts = {
   mixed: {
@@ -136,9 +137,7 @@ const CovidTestsStatistics = () => {
             <Chart reports={report.history.filter(r => selectedDate >= r.date)} {...selectedChartOptions} />
           </div>
           {selectedStat !== 'mixed' &&
-            <div className='mixed-chart-container'>
-              <div className='mixed-chart-button' onClick={() => setSelectedStat('mixed')}>Afficher le cumul</div>
-            </div>}
+            <Button title='Afficher le cumul' onClick={() => setSelectedStat('mixed')} isMobileDevice={isMobileDevice} />}
         </>
       )}
       {report && <CovidTestsAgeChart reports={report.history.filter(r => selectedDate >= r.date)} />}
@@ -184,35 +183,6 @@ const CovidTestsStatistics = () => {
 
         .chart-container {
           margin: ${isMobileDevice ? '0 0.2em' : '0 1em'};
-        }
-
-        .mixed-chart-container {
-          border-bottom: 1px solid ${colors.darkBlue};
-          border-right: 1px solid ${colors.darkBlue};
-          margin: ${isMobileDevice ? '0.5em 0.2em' : '1em'};
-        }
-
-        .mixed-chart-button {
-          display: block;
-          font-weight: bold;
-          height: 100%;
-          text-align: center;
-          background-color: ${colors.white};
-          color: ${colors.darkBlue};
-          padding: 0.4em;
-          font-size: .7em;
-          letter-spacing: .1em;
-          border: 1px solid ${colors.darkBlue};
-          text-transform: uppercase;
-          transform: translate(-.1em, -.1em);
-          transition: transform .1s ease-out;
-        }
-
-        .mixed-chart-button:hover {
-          cursor: pointer;
-          color: ${colors.white};
-          background-color: ${colors.darkBlue};
-          transform: translate(0px, 0px);
         }
         `}</style>
     </>
