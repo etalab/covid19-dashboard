c2d6d5de2ecc54d1db618f64c30ece4b4cdb2312
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-06-03 17:16:55 +0200

    Add title to MaskCounters

--- components/layouts/masks/masks-counters.js
+++ components/layouts/masks/masks-counters.js
@@ -12,45 +12,44 @@ const MasksCounters = () => {
 
   return (
     <div>
-      <div>
-        <div className='counters'>
-          <Counter
-            value={43000000}
-            label='Masques grand public'
-            color='darkBlue'
-            details='Nombre de masques grand public livrés de manière hebdomadaire'
-          />
-          <Counter
-            value={companiesNb}
-            label='Entreprises productrices'
-            color='darkBlue'
-            details='Nombre d’entreprises productrices de masques'
-          />
-        </div>
-        <h3>Prototypes</h3>
-        <div className='counters'>
-          <Counter
-            value={1815}
-            label='Protoypes testés'
-            color='darkBlue'
-            details='Nombre de prototypes de masque testés'
-          />
-          <Counter
-            value={1207}
-            label='Prototypes validés'
-            color='darkBlue'
-            details='Nombre de prototypes de masque testés comme répondant aux spécifications de l’État'
-          />
-          <Counter
-            value={430}
-            label='Entreprises avec prototypes validés'
-            color='darkBlue'
-            details='Nombre d’entreprises ayant proposé des prototypes testés comme répondant aux spécifications de l’Etat'
-          />
-        </div>
+      <h3>Production des masques grand public</h3>
+      <div className='counters'>
+        <Counter
+          value={43000000}
+          label='Masques grand public'
+          color='darkBlue'
+          details='Nombre de masques grand public livrés de manière hebdomadaire'
+        />
+        <Counter
+          value={companiesNb}
+          label='Entreprises productrices'
+          color='darkBlue'
+          details='Nombre d’entreprises productrices de masques'
+        />
+      </div>
+      <h4>Prototypes</h4>
+      <div className='counters'>
+        <Counter
+          value={1815}
+          label='Protoypes testés'
+          color='darkBlue'
+          details='Nombre de prototypes de masque testés'
+        />
+        <Counter
+          value={1207}
+          label='Prototypes validés'
+          color='darkBlue'
+          details='Nombre de prototypes de masque testés comme répondant aux spécifications de l’État'
+        />
+        <Counter
+          value={430}
+          label='Entreprises avec prototypes validés'
+          color='darkBlue'
+          details='Nombre d’entreprises ayant proposé des prototypes testés comme répondant aux spécifications de l’Etat'
+        />
       </div>
       <style jsx>{`
-        h3 {
+        h3, h4 {
           text-align: center;
         }
 
