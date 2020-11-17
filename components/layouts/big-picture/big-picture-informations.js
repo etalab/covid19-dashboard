7ffa232a82d40b3e4f6149a034a862f1d189248f
tmerlier <theophile.merliere@gmail.com>
2020-11-17 18:34:10 +0100

    Update big picture informations

--- components/layouts/big-picture/big-picture-informations.js
+++ components/layouts/big-picture/big-picture-informations.js
@@ -9,10 +9,10 @@ const BigPictureInformations = React.memo(() => (
     </p>
     <h3>Informations</h3>
     <p>
-      L’information officielle sur la progression de l’épidémie en France est consolidée par <a target='_top' href='https://www.santepubliquefrance.fr'>Santé publique France</a>. L’agence propose un <a target='_top' href='https://www.santepubliquefrance.fr/maladies-et-traumatismes/maladies-et-infections-respiratoires/infection-a-coronavirus/articles/infection-au-nouveau-coronavirus-sars-cov-2-covid-19-france-et-monde'>point épidémiologique quotidien</a>, qui comprend les chiffres-clés nationaux. Elle propose également des <a target='_top' href='https://www.data.gouv.fr/fr/organizations/sante-publique-france/'>données relatives à l’épidémie plus précises</a> sur la plateforme <a target='_top' href='https://www.data.gouv.fr'>www.data.gouv.fr</a>.
+      Les données officielles sur la progression de l’épidémie en France sont consolidées par le Ministère des Solidarités et de la <a target='_top' href='https://www.santepubliquefrance.fr'>Santé et Santé publique France</a>. L’ensemble des données publiées sur ce tableau de bord peuvent être téléchargées sur la plateforme des données publiques <a href='www.data.gouv.fr'>www.data.gouv.fr</a>.
     </p>
     <p>
-      Cet outil <a target='_top' href='https://github.com/etalab/covid19-dashboard'>dont le code source est libre</a>, développé sous l’impulsion d’<a target='_top' href='https://www.etalab.gouv.fr'>Etalab</a>, au sein de la <a target='_top' href='https://www.numerique.gouv.fr/dinum/'>direction interministérielle du numérique</a>, propose une vision consolidée des données officielles disponibles.
+      Cet outil, <a target='_top' href='https://github.com/etalab/covid19-dashboard'>dont le code source est libre</a>, est développé sous l’impulsion d’<a target='_top' href='https://www.etalab.gouv.fr'>Etalab</a>, au sein de la direction interministérielle du numérique.
     </p>
     <p>Pour obtenir d’autres informations relatives à l’épidémie, il est possible de consulter <a target='_top' href='https://www.gouvernement.fr/info-coronavirus'>la page d’information du Gouvernement sur le COVID-19</a>.</p>
 
