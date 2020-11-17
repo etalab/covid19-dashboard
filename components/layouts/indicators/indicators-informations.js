b83e7819d6a7baf5828d4568a8dc36b57ad32c43
tmerlier <theophile.merliere@gmail.com>
2020-11-17 18:52:32 +0100

    Update indicators informations

---
+++ components/layouts/indicators/indicators-informations.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import BigPictureInformations from '../big-picture/big-picture-informations'
+
+export default () => (
+  <BigPictureInformations>
+    Pour le taux d’incidence, le R effectif et le taux de positivité, les indicateurs sont publiés avec quelques jours de décalage, le temps que les tests RT-PCR soient analysés et les résultats centralisés.
+  </BigPictureInformations>
+)
