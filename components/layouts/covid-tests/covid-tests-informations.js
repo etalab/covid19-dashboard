adfa33165475739196cb2e86d86c494f29d00c76
tmerlier <theophile.merliere@gmail.com>
2020-11-19 13:41:54 +0100

    Typo

--- components/layouts/covid-tests/covid-tests-informations.js
+++ components/layouts/covid-tests/covid-tests-informations.js
@@ -6,7 +6,7 @@ const TestsInformations = React.memo(() => (
   <div className='informations'>
     <h3>Informations</h3>
     <p>
-      Les données officielles sur la progression de l’épidémie en France sont consolidées par le Ministère des Solidarités et de la <a target='_top' href='https://www.santepubliquefrance.fr'>Santé et Santé publique France</a>. L’ensemble des données publiées sur ce tableau de bord peuvent être téléchargées sur la plateforme des données publiques <a href='https://www.data.gouv.fr'>www.data.gouv.fr</a>.
+      Les données officielles sur la progression de l’épidémie en France sont consolidées par le Ministère des Solidarités et de la <a target='_top' href='https://www.santepubliquefrance.fr'>Santé et Santé publique France</a>. L’ensemble des données publiées sur ce tableau de bord peut être téléchargées sur la plateforme des données publiques <a href='https://www.data.gouv.fr'>www.data.gouv.fr</a>.
     </p>
     <p>
       Les données de suivi des tests sont issues du système d’information de dépistage (SI-DEP), dont l’objectif est de remonter les données de tests (RT-PCR) réalisés par l’ensemble des laboratoires de ville et établissements hospitaliers concernant le SARS-COV2. Compte-tenu du temps d’analyse des tests et de collecte des résultats, les données reflètent la situation à J-3. Elles sont généralement sous-estimées, car l’ensemble des résultats de tests à J-3 ne sont pas remontés à la date J.
