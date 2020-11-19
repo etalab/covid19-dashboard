21796cf59c76e05a140cf12f7e4619cdbe52a1a3
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-11-19 21:50:41 +0100

    Fix some indicators parsing

--- scripts/prepare-data/index.js
+++ scripts/prepare-data/index.js
@@ -243,6 +243,12 @@ async function loadTroisLabosTests() {
   return [...departementsReports, ...regionsReports, ...franceReports]
 }
 
+function parseIndicator(value) {
+  if (value !== 'NA') {
+    return Number.parseFloat(value)
+  }
+}
+
 async function loadIndicateurs(depUrl, franceUrl) {
   const csvOptions = {
     filter: row => row.extract_date === TODAY || row.extract_date < TODAY
@@ -253,13 +259,13 @@ async function loadIndicateurs(depUrl, franceUrl) {
       return {
         date: row.extract_date,
         code: `DEP-${row.departement}`,
-        tauxIncidence: Number.parseFloat(row.tx_incid),
+        tauxIncidence: parseIndicator(row.tx_incid),
         tauxIncidenceColor: row.tx_incid_couleur,
-        tauxReproductionEffectif: Number.parseFloat(row.R),
+        tauxReproductionEffectif: parseIndicator(row.R),
         tauxReproductionEffectifColor: row.R_couleur,
-        tauxOccupationRea: Number.parseFloat(row.taux_occupation_sae),
+        tauxOccupationRea: parseIndicator(row.taux_occupation_sae),
         tauxOccupationReaColor: row.taux_occupation_sae_couleur,
-        tauxPositiviteTests: Number.parseFloat(row.tx_pos),
+        tauxPositiviteTests: parseIndicator(row.tx_pos),
         tauxPositiviteTestsColor: row.tx_pos_couleur,
         sourceType: 'ministere-sante'
       }
@@ -294,10 +300,10 @@ async function loadIndicateurs(depUrl, franceUrl) {
       return {
         date: row.extract_date,
         code: 'FRA',
-        tauxIncidence: Number.parseFloat(row.tx_incid),
-        tauxReproductionEffectif: Number.parseFloat(row.R),
-        tauxOccupationRea: Number.parseFloat(row.taux_occupation_sae),
-        tauxPositiviteTests: Number.parseFloat(row.tx_pos),
+        tauxIncidence: parseIndicator(row.tx_incid),
+        tauxReproductionEffectif: parseIndicator(row.R),
+        tauxOccupationRea: parseIndicator(row.taux_occupation_sae),
+        tauxPositiviteTests: parseIndicator(row.tx_pos),
         sourceType: 'ministere-sante'
       }
     }).value()
