e9d2f8c49fd2c27d4079a208d528d0f5bcb2ee8c
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-02-02 21:45:47 +0100

    vaccins: change separateur csv

--- scripts/prepare-data/vaccination.js
+++ scripts/prepare-data/vaccination.js
@@ -79,7 +79,7 @@ function computeStockRecord(scopedRows, {code, nom}) {
 }
 
 async function fetchStocksFrance() {
-  const rows = await fetchCsv('https://www.data.gouv.fr/fr/datasets/r/519e2699-27d2-47c0-840b-81dbb30d4318', {separator: ';'})
+  const rows = await fetchCsv('https://www.data.gouv.fr/fr/datasets/r/519e2699-27d2-47c0-840b-81dbb30d4318', {separator: ','})
 
   return chain(rows)
     .groupBy('date')
