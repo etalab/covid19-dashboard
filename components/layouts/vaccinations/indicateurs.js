a8e329b4c033f35b1bc16ab9f8b65c536a073752
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-02-02 15:08:40 +0100

    vaccins: edl

--- components/layouts/vaccinations/indicateurs.js
+++ components/layouts/vaccinations/indicateurs.js
@@ -1,29 +1,29 @@
 const indicateurs = [
   {
     name: 'cumulPremieresInjections',
-    label: 'premières doses injectées (cumul)',
-    details: 'Nombre total de premières doses de vaccin injectées à J-1',
+    label: 'premières doses injectées (au total)',
+    details: 'Nombre total de premières doses de vaccin injectées (données à J-1 ou J-2)',
     color: 'greenSoft',
     radiusBounds: [0, 10, 800, 70]
   },
   {
     name: 'totalPrisesRendezVousSemaine',
-    label: 'rendez-vous pris par semaine (cumul)',
-    details: 'Nombre de rendez-vous pris par semaine dans les centres de vaccination',
+    label: 'rendez-vous prévus cette semaine',
+    details: 'Nombre de rendez-vous prévus cette semaine dans les centres de vaccination',
     color: 'darkGrey',
     radiusBounds: [0, 10, 300, 70]
   },
   {
     name: 'prisesRendezVousSemaineRang1',
-    label: 'rendez-vous pris par semaine pour une première injection',
-    details: 'Nombre de rendez-vous pris par semaine dans les centres de vaccination pour l’injection d’une première dose',
+    label: 'rendez-vous prévus cette semaine pour une première injection',
+    details: 'Nombre de rendez-vous prévus cette semaine dans les centres de vaccination pour l’injection d’une première dose',
     color: 'blueSoft',
     radiusBounds: [0, 10, 300, 70]
   },
   {
     name: 'prisesRendezVousSemaineRang2',
-    label: 'rendez-vous pris par semaine pour une seconde injection',
-    details: 'Nombre de rendez-vous pris par semaine dans les centres de vaccination pour l’injection d’une seconde dose',
+    label: 'rendez-vous prévus cette semaine pour une seconde injection',
+    details: 'Nombre de rendez-vous prévus cette semaine dans les centres de vaccination pour l’injection d’une seconde dose',
     color: 'purple',
     radiusBounds: [0, 10, 300, 70]
   }
