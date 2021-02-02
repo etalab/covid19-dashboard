0eab73a74c1c0176722e687ad855204ace47697e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-02-02 18:59:23 +0100

    vaccins: edl vaccins

--- components/layouts/vaccins/vaccins-informations.js
+++ components/layouts/vaccins/vaccins-informations.js
@@ -5,13 +5,19 @@ import colors from '../../../styles/colors'
 const VaccinsInformations = React.memo(() => (
   <div className='informations'>
     <h3>Informations</h3>
-    <p>
-      Les données officielles sur la progression de l’épidémie en France sont consolidées par le Ministère des Solidarités et de la Santé et <a target='_top' href='https://www.santepubliquefrance.fr'>Santé publique France</a>. L’ensemble des données publiées sur ce tableau de bord peut être téléchargées sur la plateforme des données publiques <a href='https://www.data.gouv.fr'>www.data.gouv.fr</a>.
-    </p>
-    <p>
-      Cet outil, <a target='_top' href='https://github.com/etalab/covid19-dashboard'>dont le code source est libre</a>, est développé sous l’impulsion d’<a target='_top' href='https://www.etalab.gouv.fr'>Etalab</a>, au sein de la direction interministérielle du numérique.
-    </p>
-    <p>Pour obtenir d’autres informations relatives à l’épidémie, il est possible de consulter <a target='_top' href='https://www.gouvernement.fr/info-coronavirus'>la page d’information du Gouvernement sur le COVID-19</a>.</p>
+
+    <h4>Précision concernant les stocks de vaccins</h4>
+
+    <p>Compte-tenu des conditions de conservation de doses Pfizer, deux flux ont été créés :</p>
+
+    <ul>
+      <li>Le flux A correspond aux doses transportées vers une des 6 plateformes de stockage Santé Publique France. En fonction des demandes remontées, les doses sont ensuite transportées vers les officines rattachées aux EHPAD puis livrées aux EHPAD.</li>
+      <li>Le flux B correspond aux doses transportées toutes les semaines vers une centaine d’établissements de santé publics. Ces doses sont ensuite livrées dans les centres de vaccination, dans les EHPAD rattachés au flux B par l’ARS et dans les FAM et MAS. Les établissements de santé pivots sont également des centres de vaccination, ils conservent donc une partie des doses pour pouvoir vacciner.</li>
+    </ul>
+
+    <p>En raison du faible nombre de doses Moderna reçues à date, ces vaccins ont été livrés en priorité à des établissements de santé situés dans des régions à forte circulation épidémique. Les doses stockées dans les établissements pivots sont livrées dans les centres de vaccination.</p>
+
+    <p>Ces données sont publiées et accessibles sur la plateforme <a href='https://www.data.gouv.fr'>www.data.gouv.fr</a>.</p>
 
     <style jsx>{`
       .informations {
